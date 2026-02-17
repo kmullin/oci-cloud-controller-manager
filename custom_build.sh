de51b0019c2659a128b7ba1c13c6f931c71f6547
Kevin Mullin <kevin@kpmullin.com>
2026-02-16 20:39:36 -0800

    fix build process in custom_image

--- custom_build.sh
+++ custom_build.sh
@@ -8,6 +8,7 @@ VERSION=${VERSION:-$2}
 export CI_IMAGE_REGISTRY=$REGISTRY/oracle
 export OSS_REGISTRY=$REGISTRY/oracle
 export VERSION
+export DOCKER_BUILDKIT=0
 
 # build it and push it
 make build image docker-push-all
